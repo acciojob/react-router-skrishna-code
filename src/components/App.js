873a3e37fd304dda82e434e78fa8de7f4ddb4b67
skrishna-code <krishnasutar026@gmail.com>
2026-09-24 09:55:20 +0530

    Refactor App component to include routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,25 @@
-
 import React from "react";
-import './../styles/App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 
-const App = () => {
+import Home from "./Home";
+import About from "./About";
+import Navigation from "./Navigation";
+
+function App() {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+    <BrowserRouter>
+      <Navigation />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
